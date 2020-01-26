0744edfd0febf891952fb8478cd8dd4a79cf3d1e
Daniar <sunrisedevelops@gmail.com>
2020-01-26 19:11:03 +0600

    first commit

---
+++ app/src/main/java/com/geektech/taskapp/FormActivity.java
@@ -0,0 +1,32 @@
+package com.geektech.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class FormActivity extends AppCompatActivity {
+
+    private EditText editTitle;
+    private EditText editDesc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        editTitle = findViewById(R.id.editTitle);
+        editDesc = findViewById(R.id.editDesc);
+    }
+
+    public void onClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String desc = editDesc.getText().toString().trim();
+        Intent intent = new Intent();
+        Task task = new Task(title, desc);
+        intent.putExtra("task", task);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
